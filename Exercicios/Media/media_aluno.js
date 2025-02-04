b12a421d4e93d0803342dfdeca33fa37bc0d411e
IanS04 <107213634+IanS04@users.noreply.github.com>
2025-02-04 14:07:14 -0300

    Update media_aluno.js

--- Exercicios/Media/media_aluno.js
+++ Exercicios/Media/media_aluno.js
@@ -1,5 +1,5 @@
 /*
-1) Faça um programa que receba a média de um aluno.
+Faça um programa que receba a média de um aluno.
 Caso a media seja menor que 5 imprima Reprovado
 Caso a média seja maior ou igual a 5 e menor que 7 imprima Recuperação
 Caso a média seja maior igual a 7 imprima Aprovado
@@ -7,30 +7,12 @@ Caso a média seja maior igual a 7 imprima Aprovado
 
 const { gets, print} = require('./notas');
 
-const nota = get();
-const media = [];
+const media = gets();
 
-for (let i = 0; i < nota; i++) {
-    const media = gets();
-    media.push(media);
-}
-
-let somaNotas
-
-for (let i = 0; i < media.length; i++) {
-    const valor = media[i];
-    if(media > 0){
-        somaNotas = media + media;
-    }
-}
-const Resultado = (somaNotas / nota);
-
-if (Resultado < 5) {
-    console.log('Reprovado');
-} else if (Resultado >= 5 && Resultado < 7) {
-    console.log('Recuperação');
+if (media < 5) {
+    print('Reprovado');
+} else if (media >= 5 && media < 7){
+    print('Reculperação');
 } else {
-    console.log('Aprovado');
+    print('Aprovado');
 }
-
-print(Resultado);
